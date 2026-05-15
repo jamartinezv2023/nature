abe4dbf44c01452e559e2599d1293d72a60eb3a2
José Alfredo Martínez Valdés <jose.martinez7@udea.edu.co>
2026-05-15 14:27:21 -0500

    ci: add github actions workflow and phpunit testing setup

---
+++ tests/Unit/AuthServiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class AuthServiceTest extends TestCase
+{
+    public function testTrueIsTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
